76933ed0e46c01cf544312cd03cb0d7835a18c84
danknjoroge <dank8505@gmail.com>
2022-02-26 21:09:22 +0300

    added get news functon

--- app/request.py
+++ app/request.py
@@ -14,3 +14,19 @@ apiKey = app.config['NEWS_API_KEY']
 base_url =app.config["NEWS_API_BASE_URL"]
 
 article_base_url = app.config["ARTICLE_API_BASE_URL"]
+
+def get_news(category):
+    '''function that gets json response to url format'''
+    get_news_url = base_url.format(category,apiKey)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['sources']:
+            news_results_list = get_news_response['sources']
+            news_results = process_results(news_results_list)
+
+    return news_results
