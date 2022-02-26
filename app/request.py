645c7d96237a16e19933953228d0e72f6c330500
danknjoroge <dank8505@gmail.com>
2022-02-26 21:10:29 +0300

    added process results function

--- app/request.py
+++ app/request.py
@@ -30,3 +30,22 @@ def get_news(category):
             news_results = process_results(news_results_list)
 
     return news_results
+
+
+def process_results(news_list):
+    '''Function  that processes the movie result and transform them to a list of Objects'''
+    news_results = []
+    for news_item in news_list:
+        id = news_item.get('id')
+        name = news_item.get('name')
+        description = news_item.get('description')
+        url = news_item.get('url')
+        category = news_item.get('category')
+        language = news_item.get('language')
+        country = news_item.get('country')
+
+        
+        news_object = News(id, name,  description, url, category, language, country)
+        news_results.append(news_object)
+    return news_results
+
