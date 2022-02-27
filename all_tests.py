16c2b4012845a717305eb80373d86b53a1de12b9
danknjoroge <dank8505@gmail.com>
2022-02-27 20:17:05 +0300

    added article testcases

--- all_tests.py
+++ all_tests.py
@@ -1,7 +1,8 @@
 import unittest
-from app.models import news
+from app.models import news,articles
 
 News = news.News
+Article = articles.Article
 
 class TestNews(unittest.TestCase):
     '''tests behaviours of news class'''
@@ -22,6 +23,26 @@ class TestNews(unittest.TestCase):
         self.assertEqual(self.new_news.category,"general")
         self.assertEqual(self.new_news.language,"en")
         self.assertEqual(self.new_news.country,"us")
+
+
+
+class TestArticle(unittest.TestCase):
+    '''tests behaviours for articles class'''
+    def setUp(self):
+        '''runs before every test'''
+        self.new_articles = Article("Fob Meyer", "In whose hsnd are we safe", "Its all about politics",'https://abcnews.go.com','https://img.go.com', 12/21/21, 'amazing')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_articles, Article))
+
+    def test_init(self):
+        self.assertEqual(self.new_articles.author, "Fob Meyer")
+        self.assertEqual(self.new_articles.title, "In whose hsnd are we safe")
+        self.assertEqual(self.new_articles.description, "Its all about politics")
+        self.assertEqual(self.new_articles.url, "https://abcnews.go.com")
+        self.assertEqual(self.new_articles.urlToImage, 'https://img.go.com')
+        self.assertEqual(self.new_articles.publishedAt,12/21/21)
+        self.assertEqual(self.new_articles.content, 'amazing')
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
